929dce7214f444a63cc98da21b4ac05271832a43
t-sakai-kure <t.sakai.kure@gmail.com>
2018-04-15 18:20:18 +0900

    Add javascript

---
+++ chimg.js
@@ -0,0 +1,8 @@
+$(function() {
+        $("#myImg").mouseover(function() {
+                this.src = "img/cat.jpg";
+            });
+        $("#myImg").mouseout(function() {
+                this.src = "img/Aug_2017.jpg";
+            });
+    });
